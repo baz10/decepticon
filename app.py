8b5e9c3edf359f0402a92743b957a33f9a8b9bc4
shahbazmasud <shahbaz.masud@gmail.com>
2019-02-06 15:39:44 +0000

    Fixed simsimi api issue

--- app.py
+++ app.py
@@ -1,38 +1,38 @@
 import random
 import requests
 import os
+
 from flask import Flask, request
 from pymessenger.bot import Bot
  
 app = Flask(__name__)
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
-SimsimiKey = os.environ['SimsimiKey']
+ACCESS_TOKEN = "EAADWjJgyWIABAAwnwRYxBQl3sJZBKh1h0ZBd6NrhfpX0d7ZA2KU7GufYWmtf8Y8MrF4481aBikPQpsajLlzaxbHCY83FpgZAvRCUrv1kRnnOr16OnVssw9kPywBnb0PyTGJybEdOwNQ31gtntchepLF48fM8YE3I1OnyWQW5BwZDZD"#os.environ['ACCESS_TOKEN']
+VERIFY_TOKEN = "decepticon"#os.environ['VERIFY_TOKEN']
+SimsimiKey = "90a9e3ae-527a-4055-86d8-4275ae9ccb69"#os.environ['SimsimiKey']
 bot = Bot(ACCESS_TOKEN) 
 
  
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
-    if request.method == 'GET': 
+    if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
         that confirms all requests that your bot receives came from Facebook.""" 
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
-
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
-
+        # get whatever message a user sent the bot
        output = request.get_json()
        for event in output['entry']:
           messaging = event['messaging']
           for message in messaging:
             if message.get('message'):
-
+                #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    userMessage = message['message'].get('text')
-                    response_sent_text = get_message(userMessage)
+                    response_sent_text = get_message(message['message'].get('text'))
                     send_message(recipient_id, response_sent_text)
-
+                #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
                     response_sent_nontext = get_message(1)
                     send_message(recipient_id, response_sent_nontext)
@@ -47,20 +47,17 @@ def verify_fb_token(token_sent):
 
 
 
-
-
 #chooses a random message to send to the user
 def get_message(userMessage):
     
-    URL = "http://sandbox.api.simsimi.com/request.p?key={}&lc=id&ft=1.0&text={}".format(SimsimiKey, userMessage)
+    URL = "http://sandbox.api.simsimi.com/request.p?key={}&lc=id&ft=1.0&text={}".format(SimsimiKey,userMessage)
     r = requests.get(url = URL)
     data = r.json()
-
-   # print (data)
-    if data == 509:
-       	return data['msg']
-    else:
+    if data['result'] == 1:
         return data['response']
+    else:
+        print ("Yikes! Something Went Wrong..")
+
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
